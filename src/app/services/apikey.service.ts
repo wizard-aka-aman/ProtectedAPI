72d51c22a9979c6eab9ea8602bf3eec9cff898ef
Lavneesh Rajput <lavneesh.r@quadwave.com>
2025-11-07 11:49:49 +0530

    update uel local to global

--- src/app/services/apikey.service.ts
+++ src/app/services/apikey.service.ts
@@ -9,8 +9,8 @@ export interface CreateApiDto {
 
 @Injectable({ providedIn: 'root' })
 export class ApikeyService {
-  // private base = 'https://10.0.0.2:5000/api/apikeys';
-  private base = 'https://localhost:7232/api/apikeys';
+  private base = 'https://romigas369.bsite.net/api/apikeys';
+  // private base = 'https://localhost:7232/api/apikeys';
 
   constructor(private http: HttpClient) {}
 
@@ -34,15 +34,15 @@ export class ApikeyService {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJTdWJqZWN0IiwianRpIjoiMWUwMTI1MDQtNjc3Zi00MGU3LTljMTItOGNmZGZjZGRlNTg3IiwiVXNlck5hbWUiOiJhbWFuXyIsIkVtYWlsIjoiYW1hbkBnbWFpbC5jb20iLCJGdWxsTmFtZSI6IkFtYW4gVmVybWEiLCJleHAiOjE3NjI0OTI0ODQsImlzcyI6Imh0dHBzOi8vbG9jYWxob3N0OjcyNDYvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzI0Ni8ifQ.jatgHOx1qmhRc22ZiwC1E_oizrR5wZuFEhx_Vx0Z9ug'
     });
-    // return this.http.get(`https://10.0.0.2:5000/public/verify/${id}`,{headers: header});
-    return this.http.get(`https://localhost:7232/public/verify/${id}`,{headers: header});
+    return this.http.get(`https://romigas369.bsite.net/public/verify/${id}`,{headers: header});
+    // return this.http.get(`https://localhost:7232/public/verify/${id}`,{headers: header});
   }
   getHello(token:string){
     const header = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     });
-    return this.http.get('https://localhost:7232/protected/hello' ,{headers: header});
-    // return this.http.get('https://10.0.0.2:5000/protected/hello' ,{headers: header});
+    // return this.http.get('https://localhost:7232/protected/hello' ,{headers: header});
+    return this.http.get('https://romigas369.bsite.net/protected/hello' ,{headers: header});
   }
 }
